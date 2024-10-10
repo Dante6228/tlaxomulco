36be49f542e52b8936850fae1e125f2f7f315b95
Dante <dantealejandro35@gmail.com>
2024-10-10 12:09:10 -0600

    correccion de inserción de nuevo alumno

--- php/registrarAlumno.php
+++ php/registrarAlumno.php
@@ -2,31 +2,30 @@
 
 require_once __DIR__ . "/conexion.php";
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $nombre = $_POST["nombre"];
-    $ap = $_POST["ap"];
-    $am = $_POST["am"];
-    $matricula = $_POST["matricula"];
-    $genero = $_POST["genero"];
-    $nivel_escolar = $_POST["nivel_escolar"];
-    $grado = $_POST["grado"];
-    $municipio = $_POST["municipio"];
-    $colonia = $_POST["colonia"];
-    $medio_enterado = $_POST["medio_enterado"];
-    $promocion = $_POST["promocion"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $datos = [
+        'nombre' => $_POST["nombre"],
+        'ap' => $_POST["ap"],
+        'am' => $_POST["am"],
+        'matricula' => $_POST["matricula"],
+        'genero' => $_POST["genero"],
+        'estado' => $_POST["estado"],
+        'nivel_escolar' => $_POST["nivel_escolar"],
+        'grado' => $_POST["grado"],
+        'municipio' => $_POST["municipio"],
+        'colonia' => $_POST["colonia"],
+        'medio_enterado' => $_POST["medio_enterado"],
+        'promocion' => $_POST["promocion"]
+    ];
 
-    $datos_personales = [$nombre, $ap, $am, $matricula, $genero];
-    $direccion = [$municipio, $colonia];
-    $extra_info = [$nivel_escolar, $grado, $medio_enterado, $promocion];
-
-    registrar($datos_personales, $direccion, $extra_info);
+    registrar($datos);
 
 } else {
     header("Location: ../index.php?mensaje=err1");
     exit();
 }
 
-function registrar($datos_personales, $direccion, $extra_info) {
+function registrar($datos) {
     try {
         $pdo = Conexion::connection();
 
@@ -34,9 +33,22 @@ function registrar($datos_personales, $direccion, $extra_info) {
             throw new UnexpectedValueException("Error en la conexión a la base de datos");
         }
 
-        $stmt = $pdo->prepare("INSERT INTO alumno (nombre, Ap, Am, matricula, genero, nivel, grado_id, municipio, colonia, medio_enterado, promocion) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
-        
-        $stmt->execute(array_merge($datos_personales, $direccion, $extra_info));
+        $stmt = $pdo->prepare("INSERT INTO alumno (nombre, Ap, Am, matricula, genero, estado, nivel, grado_id, municipio, colonia, medio_enterado, promocion) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
+
+        $stmt->execute([
+            $datos['nombre'],
+            $datos['ap'],
+            $datos['am'],
+            $datos['matricula'],
+            $datos['genero'],
+            $datos['estado'],
+            $datos['nivel_escolar'],
+            $datos['grado'],
+            $datos['municipio'],
+            $datos['colonia'],
+            $datos['medio_enterado'],
+            $datos['promocion']
+        ]);
 
         header("Location: ../alumnos.php?mensaje=registro");
         exit();
@@ -45,4 +57,3 @@ function registrar($datos_personales, $direccion, $extra_info) {
         exit();
     }
 }
-
