87252920e1f0342692a810493495f6c9b6c0d4b8
Dante <dantealejandro35@gmail.com>
2024-11-13 12:48:24 -0600

    Continuación de importación de excel

--- php/excel/importar.php
+++ php/excel/importar.php
@@ -105,6 +105,8 @@ if ($data) {
             exit();
         }
 
+        echo  "<h1>" . "Alumno ", $i . "</h1>";
+
         /////////////////////////////////////////////////////////////////
         // INSERCIÓN DE CICLO //
         /////////////////////////////////////////////////////////////////
@@ -144,8 +146,6 @@ if ($data) {
                     ':ciclo_id' => $cicloId,
                 ]);
             }
-
-            echo "Ciclo: " . $ciclo . " creado correctamente <br>";
         }
 
         /////////////////////////////////////////////////////////////////
@@ -190,10 +190,14 @@ if ($data) {
             $gradoId = $gradoData['id'];
             echo "Grado existente: $grado con ID: $gradoId<br>";
         } else{
-            echo "Error: grado no existente <br>";
+            die("Error: grado no existente");
         }
 
-        // Conseguir nivel_grado_ciclo
+        /////////////////////////////////////////////////////////////////
+        // OBTENCIÓN DE NIVEL_GRADO_CICLO //
+        /////////////////////////////////////////////////////////////////
+
+        // Verificar si el nivel_grado_ciclo ya existe en la base de datos
         $query = "SELECT id FROM nivel_grado_ciclo WHERE nivel_educativo_id = :nivelId AND grado_id = :gradoId AND ciclo_id = :cicloId";
         $stmt = $pdo->prepare($query);
         $stmt->bindParam(':nivelId', $nivelId2);
@@ -210,13 +214,155 @@ if ($data) {
             echo "a";
         }
 
-        echo "<br><strong>Datos nivel, grado y ciclo extraidos correctamete</strong> <hr>";
+        echo "<br><strong>Datos nivel, grado y ciclo extraidos correctamete</strong><br><br>";
 
+        /////////////////////////////////////////////////////////////////
+        // OBTENCIÓN DE COLONIA Y MUNICIPIO //
+        /////////////////////////////////////////////////////////////////
+
+        // Verificar si el municipio ya existe en la base de datos
+        $query = "SELECT id FROM municipio WHERE descripcion = :descripcion";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':descripcion', $municipio);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            // El municipio ya existe; obtener el ID
+            $municipioData = $stmt->fetch(PDO::FETCH_ASSOC);
+            $municipioId = $municipioData['id'];
+            echo "Municipio existente: $municipio con ID: $municipioId <br>";
+        } else{
+            $query = "INSERT INTO municipio (descripcion) VALUES (:municipio)";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([':municipio' => $municipio]);
+            $municipioId = $pdo->lastInsertId();
+            echo "Municipio $municipio creado con ID: $municipioId <br>";
+        }
+
+        // Verificar si la colonia ya existe en la base de datos
+        $query = "SELECT id FROM colonia WHERE descripcion = :descripcion AND municipio_id = :municipioId";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':descripcion', $colonia);
+        $stmt->bindParam(':municipioId', $municipioId);
+        $stmt->execute();
+        
+        if ($stmt->rowCount() > 0) {
+            // La colonia ya existe; obtener el ID
+            $coloniaData = $stmt->fetch(PDO::FETCH_ASSOC);
+            $coloniaId = $coloniaData['id'];
+            echo "Colonia existente: $colonia con ID: $coloniaId <br>";
+        } else{
+            $query = "INSERT INTO colonia (descripcion, municipio_id) VALUES (:colonia, :municipioId)";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([':colonia' => $colonia, ':municipioId' => $municipioId]);
+            $coloniaId = $pdo->lastInsertId();
+            echo "Colonia $colonia creada con ID: $coloniaId <br>";
+        }
+
+        /////////////////////////////////////////////////////////////////
+        // OBTENCIÓN DE GÉNERO //
+        /////////////////////////////////////////////////////////////////
+
+        // Verificar si el género ya existe en la base de datos
+        $query = "SELECT id FROM genero WHERE descripcion = :descripcion";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':descripcion', $genero);
+        $stmt->execute();
+        
+        if ($stmt->rowCount() > 0) {
+            // El género ya existe; obtener el ID
+            $generoData = $stmt->fetch(PDO::FETCH_ASSOC);
+            $generoId = $generoData['id'];
+            echo "Género existente: $genero con ID: $generoId <br>";
+        } else{
+            $query = "INSERT INTO genero (descripcion) VALUES (:genero)";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([':genero' => $genero]);
+            $generoId = $pdo->lastInsertId();
+            echo "Género $genero creado con ID: $generoId <br>";
+        }
+
+        /////////////////////////////////////////////////////////////////
+        // OBTENCIÓN DE PROMOCIÓN //
+        /////////////////////////////////////////////////////////////////
+
+        // Verificar si la promoción ya existe en la base de datos
+        $query = "SELECT id FROM promocion WHERE descripcion = :descripcion";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':descripcion', $promocion);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            // La promoción ya existe; obtener el ID
+            $promocionData = $stmt->fetch(PDO::FETCH_ASSOC);
+            $promocionId = $promocionData['id'];
+            echo "Promoción existente: $promocion con ID: $promocionId <br>";
+        } else{
+            $query = "INSERT INTO promocion (descripcion) VALUES (:descripcion)";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([':descripcion' => $promocion]);
+            $promocionId = $pdo->lastInsertId();
+            echo "Promoción $promocion creada con ID: $promocionId <br>";
+        }
+
+        /////////////////////////////////////////////////////////////////
+        // OBTENCIÓN DE MEDIO ENTERADO //
+        /////////////////////////////////////////////////////////////////
+
+        // Verificar si el medio de enterado ya existe en la base de datos
+        $query = "SELECT id FROM medio_enterado WHERE descripcion = :descripcion";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':descripcion', $medioEnterado);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            // El medio de enterado ya existe; obtener el ID
+            $medioEnteradoData = $stmt->fetch(PDO::FETCH_ASSOC);
+            $medioEnteradoId = $medioEnteradoData['id'];
+            echo "Medio de enterado existente: $medioEnterado con ID: $medioEnteradoId <br>";
+        } else{
+            $query = "INSERT INTO medio_enterado (descripcion) VALUES (:descripcion)";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([':descripcion' => $medioEnterado]);
+            $medioEnteradoId = $pdo->lastInsertId();
+            echo "Medio de enterado $medioEnterado creado con ID: $medioEnteradoId <br>";
+        }
+
+        /////////////////////////////////////////////////////////////////
+        // OBTENCIÓN DE ESTADO //
+        /////////////////////////////////////////////////////////////////
+
+        // Verificar si el estado ya existe en la base de datos
+        $query = "SELECT id FROM estado WHERE descripcion = :descripcion";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':descripcion', $estado);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            // El estado ya existe; obtener el ID
+            $estadoData = $stmt->fetch(PDO::FETCH_ASSOC);
+            $estadoId = $estadoData['id'];
+            echo "Estado existente: $estado con ID: $estadoId <br>";
+        } else{
+            $query = "INSERT INTO estado (descripcion) VALUES (:descripcion)";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([':descripcion' => $estado]);
+            $estadoId = $pdo->lastInsertId();
+            echo "Estado $estado creado con ID: $estadoId <br>";
+        }
+
+        echo "<br><strong>Datos genero, municipio, colonia, estado, medio enterado y promoción extraidos correctamete</strong><br><br>";
+
+        /////////////////////////////////////////////////////////////////
+        // INSERCIÓN DE ALUMNO //
+        /////////////////////////////////////////////////////////////////
+
+        $query = "INSERT INTO alumno () VALUES ()";
+
+        echo "<hr>";
         
     }
-
-
-    echo "<br>Datos importados exitosamente.";
+    
 } else {
     echo "No hay datos para importar.";
 }
