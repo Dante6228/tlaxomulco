6274b349f8a39f49afeb52133f63f7da0cccc0b4
Dante <dantealejandro35@gmail.com>
2024-12-08 18:29:39 -0600

    Cambios al header excepto en actualizar_alumno

--- alumnos.php
+++ alumnos.php
@@ -32,13 +32,34 @@ $nivel = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="css/alumnos.css">
     <link rel="stylesheet" href="css/header.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Alumnos</title>
 </head>
 <body>
     <header>
-        <h1>Alumnos</h1>
-        <a href="Bienvenida.php" class="botonInicio">Inicio</a>
-        <a href="usuario.php"><img src="img/usuario.png" alt="Perfil"></a>
+        <div class="logo">
+            <img src="img/logo.png" alt="Logo del instituto Tlaxomulco">
+            <h1>Instituto Tlaxomulco</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="Bienvenida.php">Inicio</a></li>
+                <li><a href="alumnos.php">Alumnos</a></li>
+                <li><a href="Datos.php">Datos</a></li>
+                <li><a href="consulta.php">Consulta específica</a></li>
+            </ul>
+        </nav>
+        <div class="saludoContainer">
+            <a href="usuario.php">
+                <img src="img/usuario.png" alt="Foto de usuario">
+            </a>
+            <div class="saludo">
+                <h2>Hola</h2>
+                <p><?php echo $_SESSION["nombre"]?></p>
+            </div>
+        </div>
     </header>
 
     <main>
